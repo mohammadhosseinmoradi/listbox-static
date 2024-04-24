7a729a6d0ba1059a5850cb9d72e1fcba8f336e7d
mohammadhosseinmoradi <mohammadhosseinmoradi.545@gmail.com>
2024-04-24 19:47:33 +0330

    Add listbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,103 @@
-import Image from "next/image";
+"use client"
+
+import {Field, Label, Listbox} from "@headlessui/react";
+import {useState} from "react";
 
 export default function Home() {
+  const [value, setValue] = useState("u002");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="p-12">
+      <Field>
+        <Label>
+          Select user:
+        </Label>
+        <p className='text-xs text-neutral-500'>
+          I want render listbox static, not want open it in popover.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <Listbox as='div' value={value} onChange={setValue} className='w-72 border rounded-lg overflow-hidden mt-2'>
+          <Listbox.Options
+            // It's ok when refresh it with key.
+            // key={value}
+            as="li" static
+            className='flex flex-col gap-2 h-64 overflow-y-auto p-2 list-none'>
+            {
+              users.map(user => {
+                return <Listbox.Option
+                  as='li'
+                  key={user.id}
+                  value={user.id}
+                  className='hover:bg-neutral-100 rounded-lg p-2 cursor-pointer data-[selected]:bg-blue-500 data-[selected]:text-white'
+                >
+                  <h5 className='font-medium text-sm'>
+                    {
+                      user.name
+                    }
+                  </h5>
+                  <p className='opacity-50 text-xs mt-1'>
+                    {
+                      user.email
+                    }
+                  </p>
+                </Listbox.Option>
+              })
+            }
+          </Listbox.Options>
+        </Listbox>
+      </Field>
     </main>
   );
 }
+
+const users = [
+  {
+    id: "u001",
+    name: "Alice Smith",
+    email: "alice.smith@example.com",
+  },
+  {
+    id: "u002",
+    name: "Bob Johnson",
+    email: "bob.johnson@example.com",
+  },
+  {
+    id: "u003",
+    name: "Charlie Williams",
+    email: "charlie.williams@example.com",
+  },
+  {
+    id: "u004",
+    name: "Diana Moore",
+    email: "diana.moore@example.com",
+  },
+  {
+    id: "u005",
+    name: "Ethan Miller",
+    email: "ethan.miller@example.com",
+  },
+  {
+    id: "u006",
+    name: "Fiona Garcia",
+    email: "fiona.garcia@example.com",
+  },
+  {
+    id: "u007",
+    name: "George Brown",
+    email: "george.brown@example.com",
+  },
+  {
+    id: "u008",
+    name: "Hannah Walker",
+    email: "hannah.walker@example.com",
+  },
+  {
+    id: "u009",
+    name: "Isaac Lee",
+    email: "isaac.lee@example.com",
+  },
+  {
+    id: "u010",
+    name: "Jasmine Hernandez",
+    email: "jasmine.hernandez@example.com",
+  },
+];
